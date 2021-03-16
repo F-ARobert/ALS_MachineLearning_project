6ad0fd22df96510432dd88aff41b6fe0400aef2d
F-ARobert <felix-antoine.robert.1@etsmtl.net>
2021-03-15 23:36:54 -0400

    Need to find a way to save large data

--- main.m
+++ main.m
@@ -5,12 +5,15 @@ clear variables;
 
 % Load data
 data = readtable('data.csv');
+% data = datastore('data.csv');
+% data = tall(data);
 %%
 % Create list of img ids
 ids = data.('ImgID'); % use {} to access strings
 
 %% Constants
 new_z_dim = 128;
+new_size = [512 512 new_z_dim];
 % Blurring window size and Padding value
 win_blur = 5;
 pad = 'replicate';
@@ -18,8 +21,8 @@ sigma = 0.5;
 i = 1;
 %% Cycle through each image and apply preprocessing
 
-% for i = 1:length(ids)
-    tic
+for i = 1:length(ids)
+     tic
     % Load image
     name = ids{i};
     name = strcat('images/', name, '.TIF');
@@ -27,6 +30,7 @@ i = 1;
         img = Tiff(name);
     catch ME
         display ('Error opening' + name + 'image');
+        continue;
     end
     
     % Create imag array
@@ -72,17 +76,43 @@ i = 1;
     mask_R = imfill(mask_R,6, 'holes');
     mask_G = imfill(mask_G,6, 'holes');
     
-    %% Step 5: Apply labels to number connected regions
+    %% Step 5: Remove objects on border
+    margin = 10; % Represents roughly 10% of image
+    croppedImg_R = imcrop3(mask_R, [margin margin margin...
+        new_size(1)-1-2*margin...
+        new_size(2)-1-2*margin...
+        new_size(3)-1-2*margin]);
+    croppedImg_G = imcrop3(mask_G, [margin margin margin...
+        new_size(1)-1-2*margin...
+        new_size(2)-1-2*margin...
+        new_size(3)-1-2*margin]);
+    
+    mask_R = imclearborder(croppedImg_R);
+    mask_G = imclearborder(croppedImg_G);
+    mask_R = logical(padarray(mask_R, [margin margin margin], 'both'));
+    mask_G = logical(padarray(mask_G, [margin margin margin], 'both'));
+    
+    %% Step 6: Apply labels to number connected regions
     [L_R, n_R] = bwlabeln(mask_R, 6);
     [L_G, n_G] = bwlabeln(mask_G, 6);% Creates many labels
-    display(n_R)
-    display(n_G)
+%     display(n_R)
+%     display(n_G)
     
-    %% Step 6: Need to cluster these regions
+    %% Step 7: Apply mask over original image
+    new_img(:,:,:,1) = resized_img(:,:,:,1) .* uint8(mask_R);
+    new_img(:,:,:,2) = resized_img(:,:,:,2) .* uint8(mask_G);
+    new_img(:,:,:,3) = resized_img(:,:,:,3);
+    
+    %% Clear unnecessary variables
+    clear Blurred_img resized_img croppedImg_G croppedImg_R...
+        L_G L_R mask_G mask_R;
+    toc
+    %% Step 8: Linearize image
+    tic
+    vector_image = [];
+    vector_image = tall(vector_image)
+    vector_image = reshape(new_img, 1, []);
     
     toc
-% end
+end
 %%
-new_img(:,:,:,1) = resized_img(:,:,:,1) .* uint8(mask_R);
-new_img(:,:,:,2) = resized_img(:,:,:,2) .* uint8(mask_G);
-new_img(:,:,:,3) = resized_img(:,:,:,3);
